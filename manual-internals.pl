734074a951d8da5337c06bdb41ed57999e34c2db
Kei Okada <k-okada@jsk.t.u--tokyo.ac.jp>
2016-12-19 07:15:02 +0000

    Build documents from EusLisp-dummy@c514186e859c1806fd029c6c05db31af75587bed

--- manual-internals.pl
+++ manual-internals.pl
@@ -2,63 +2,31 @@
 # Associate internals original text with physical files.
 
 
-$key = q/License/;
-$ref_files{$key} = "$dir".q|manual-node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/threadmodel/;
-$ref_files{$key} = "$dir".q|manual-node17.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/Coordinates/;
-$ref_files{$key} = "$dir".q|manual-node18.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/Contact/;
 $ref_files{$key} = "$dir".q|manual-node19.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ManipulatorModel/;
-$ref_files{$key} = "$dir".q|manual-node22.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/panellayout/;
-$ref_files{$key} = "$dir".q|manual-node24.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ObjectFig/;
+$key = q/Pointer/;
 $ref_files{$key} = "$dir".q|manual-node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/xvariables/;
-$ref_files{$key} = "$dir".q|manual-node23.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/UnixProcess/;
-$ref_files{$key} = "$dir".q|manual-node16.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/xdraw/;
-$ref_files{$key} = "$dir".q|manual-node23.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/primitive-body-creation/;
 $ref_files{$key} = "$dir".q|manual-node19.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/synchports/;
-$ref_files{$key} = "$dir".q|manual-node17.html|; 
+$key = q/PBMfile/;
+$ref_files{$key} = "$dir".q|manual-node21.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_CLOS:Keene/;
 $ref_files{$key} = "$dir".q|manual-node27.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/panelitem/;
-$ref_files{$key} = "$dir".q|manual-node24.html|; 
+$key = q/cite_CLtL2/;
+$ref_files{$key} = "$dir".q|manual-node27.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/mthread/;
+$key = q/threadmodel/;
 $ref_files{$key} = "$dir".q|manual-node17.html|; 
 $noresave{$key} = "$nosave";
 
@@ -70,55 +38,19 @@ $key = q/parathreads/;
 $ref_files{$key} = "$dir".q|manual-node17.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/PBMfile/;
-$ref_files{$key} = "$dir".q|manual-node21.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/threadobj/;
-$ref_files{$key} = "$dir".q|manual-node17.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/Drawings/;
-$ref_files{$key} = "$dir".q|manual-node20.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Eus4/;
-$ref_files{$key} = "$dir".q|manual-node27.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:peg-in-hole/;
-$ref_files{$key} = "$dir".q|manual-node19.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_CLtL/;
-$ref_files{$key} = "$dir".q|manual-node27.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/Geometry/;
-$ref_files{$key} = "$dir".q|manual-node19.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:peg-in-a-hole/;
-$ref_files{$key} = "$dir".q|manual-node19.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_CLtL2/;
-$ref_files{$key} = "$dir".q|manual-node27.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/IPC/;
 $ref_files{$key} = "$dir".q|manual-node13.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tracking/;
-$ref_files{$key} = "$dir".q|manual-node21.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_MTEus1/;
-$ref_files{$key} = "$dir".q|manual-node27.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/BodyComposition/;
+$ref_files{$key} = "$dir".q|manual-node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/panellayout/;
+$ref_files{$key} = "$dir".q|manual-node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:peg-in-a-hole/;
 $ref_files{$key} = "$dir".q|manual-node19.html|; 
 $noresave{$key} = "$nosave";
 
@@ -126,28 +58,96 @@ $key = q/Pathnames/;
 $ref_files{$key} = "$dir".q|manual-node13.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/URL-Pathnames/;
-$ref_files{$key} = "$dir".q|manual-node13.html|; 
+$key = q/UnixProcess/;
+$ref_files{$key} = "$dir".q|manual-node16.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/Pointer/;
-$ref_files{$key} = "$dir".q|manual-node4.html|; 
+$key = q/cite_CLtL/;
+$ref_files{$key} = "$dir".q|manual-node27.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/JointCoords/;
-$ref_files{$key} = "$dir".q|manual-node22.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/textviewpanel/;
-$ref_files{$key} = "$dir".q|manual-node24.html|; 
+$key = q/xdraw/;
+$ref_files{$key} = "$dir".q|manual-node23.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/ClassHierarchy/;
 $ref_files{$key} = "$dir".q|manual-node4.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_MTEus1/;
+$ref_files{$key} = "$dir".q|manual-node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ObjectFig/;
+$ref_files{$key} = "$dir".q|manual-node4.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/cite_Hirukawa:1991a/;
 $ref_files{$key} = "$dir".q|manual-node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/threadobj/;
+$ref_files{$key} = "$dir".q|manual-node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/JointCoords/;
+$ref_files{$key} = "$dir".q|manual-node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/License/;
+$ref_files{$key} = "$dir".q|manual-node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Coordinates/;
+$ref_files{$key} = "$dir".q|manual-node18.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Eus4/;
+$ref_files{$key} = "$dir".q|manual-node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/URL-Pathnames/;
+$ref_files{$key} = "$dir".q|manual-node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:peg-in-hole/;
+$ref_files{$key} = "$dir".q|manual-node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/textviewpanel/;
+$ref_files{$key} = "$dir".q|manual-node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tracking/;
+$ref_files{$key} = "$dir".q|manual-node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Drawings/;
+$ref_files{$key} = "$dir".q|manual-node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Geometry/;
+$ref_files{$key} = "$dir".q|manual-node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/xvariables/;
+$ref_files{$key} = "$dir".q|manual-node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/mthread/;
+$ref_files{$key} = "$dir".q|manual-node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/panelitem/;
+$ref_files{$key} = "$dir".q|manual-node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ManipulatorModel/;
+$ref_files{$key} = "$dir".q|manual-node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/synchports/;
+$ref_files{$key} = "$dir".q|manual-node17.html|; 
 $noresave{$key} = "$nosave";
 
 1;
